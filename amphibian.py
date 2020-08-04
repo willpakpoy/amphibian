adcabb3c3f789517711cce33fc572970cfed27e9
Will Pak Poy <30822983+willpakpoy@users.noreply.github.com>
2020-08-04 11:33:38 +1000

    Partial binding now works

--- amphibian.py
+++ amphibian.py
@@ -1,10 +1,15 @@
 import json
 import tkinter as tk
+from functools import partial
 
 store_name = 'store.json' # we define a global variable with the file name so that we can easily change it if we need to
 default_padding = 12.5
 window = tk.Tk()
 window.title('Amphibian')
+
+def clearWindow():
+    for widget in window.winfo_children():
+        widget.destroy()
 
 def singleQuestion(path): # this is what will display when we want to ask a question
     with open(store_name, 'r') as f: # code below will only run when we open the file, as f
@@ -17,16 +22,18 @@ def singleQuestion(path): # this is what will display when we want to ask a ques
         options_wrap = tk.Frame(master=question_wrap)
         for option in item['options']:
             option_list_item = [d for d in default_datastore if d['pathId'] == option][0]
-            option_button = tk.Button(master=options_wrap, text=option_list_item['preSelection'])
-            def closeThisSingleQuestion(event):
-                question_wrap.pack()
-                singleQuestion(option)
-            option_button.bind("<Button-1>", closeThisSingleQuestion)
+            option_button = tk.Button(master=options_wrap, text=option_list_item['preSelection'], command=partial(closeThisSingleQuestion, option))
             option_button.pack(side=tk.LEFT, fill=tk.BOTH)
         options_wrap.pack(padx=default_padding, pady=default_padding)
         question_wrap.pack()
         window.mainloop()
 
     print('hello!')
+
+def closeThisSingleQuestion(option):
+        clearWindow()
+        singleQuestion(option)
+
+
 
 singleQuestion(1)
