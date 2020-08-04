3abc9abcd92fff7263e2f44d0e6d0478cd484900
Will Pak Poy <30822983+willpakpoy@users.noreply.github.com>
2020-08-04 11:57:01 +1000

    Continuation vs completion, work on paths

--- amphibian.py
+++ amphibian.py
@@ -16,24 +16,19 @@ def singleQuestion(path): # this is what will display when we want to ask a ques
         question_wrap = tk.Frame(master=window)
         default_datastore = json.loads(f.read()) # load data from the json array
         item = [d for d in default_datastore if d['pathId'] == path][0] # select an item from the array that has the 'pathId' attribute being set to the 'path' argument
-        label = tk.Label(text=item['preSelection'], master=question_wrap) # create a label to be displayed in the window
-        label.pack(padx=default_padding, pady=default_padding)
-        print(item['postSelection'])
-        options_wrap = tk.Frame(master=question_wrap)
-        for option in item['options']:
-            option_list_item = [d for d in default_datastore if d['pathId'] == option][0]
-            option_button = tk.Button(master=options_wrap, text=option_list_item['preSelection'], command=partial(closeThisSingleQuestion, option))
-            option_button.pack(side=tk.LEFT, fill=tk.BOTH)
-        options_wrap.pack(padx=default_padding, pady=default_padding)
+        label = tk.Label(text=item['postSelection'], master=question_wrap) # create a label to be displayed in the window
+        label.pack(padx=default_padding, pady=default_padding) # pack the label
+        if item['type'] == 'continuation':
+            options_wrap = tk.Frame(master=question_wrap) # wrap the options in a frame
+            for option in item['options']: # this is the loop where we make the buttons
+                option_list_item = [d for d in default_datastore if d['pathId'] == option][0] # we get the path from the json file 
+                option_button = tk.Button(master=options_wrap, text=option_list_item['preSelection'], command=partial(closeThisSingleQuestion, option))
+                option_button.pack(side=tk.LEFT, fill=tk.BOTH)
+            options_wrap.pack(padx=default_padding, pady=default_padding)
         question_wrap.pack()
         window.mainloop()
-
-    print('hello!')
-
+        
 def closeThisSingleQuestion(option):
         clearWindow()
         singleQuestion(option)
-
-
-
 singleQuestion(1)
